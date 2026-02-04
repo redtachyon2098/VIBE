d65b2d8bb3c3ead6513744f4a376d5e278d811b7
redtachyon2098 <redtachyon2718@gmail.com>
2026-02-04 02:07:13 -0500

    Update modelwrapper.py

--- modelwrapper.py
+++ modelwrapper.py
@@ -3,7 +3,7 @@ from ollama import ChatResponse
 
 model = "qwen3:8b"
 
-def ollamaQuery(context):
+def ollamaQuery(context):#list of dictionaries containing roles and contents
     response: ChatResponse = chat(model = model, messages = context)
     return response['message']['content']
 
